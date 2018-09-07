6756eca1a8ac47bf4da82253b0698c6fe5778667
aarmanepal <42278225+aarmanepal@users.noreply.github.com>
2018-09-07 22:31:28 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -348,10 +348,7 @@ $(document).ready(function(){
             }
     }
 
-
-        $(document).ready(function() {
-            $('#mc_embed_signup').find('form').ajaxChimp();
-        });      
+    
 
 
 
